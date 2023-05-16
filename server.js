63671f41311936237d6676e8578fb09069484629
fabri-tech <fabriziotrevino@gmail.com>
2023-05-15 23:07:18 -0600

    reverse the change I got confused with the file

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
-const db = require('./config/connection');
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// import sequelize connection
 
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
+// sync sequelize models to the database, then turn on the server
+async function startServer() {
+  try {
+    await sequelize.sync({ force: false });
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  } catch (err) {
+    console.error('Unable to sync database:', err);
+  }
+}
+
+startServer();
